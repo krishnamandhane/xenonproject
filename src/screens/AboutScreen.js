c1cfe798192b55840d055876ad6487a2538ab531
krishnamandhane <121455720+krishnamandhane@users.noreply.github.com>
2023-04-04 21:06:37 +0530

    Frontend

---
+++ src/screens/AboutScreen.js
@@ -0,0 +1,26 @@
+import Header from '../components/Header'
+
+const AboutScreen = (props) => {
+  return (
+    <div>
+      <Header title="About" />
+   
+    <p><h3>What is it?</h3>
+    Note-taking is the process of capturing information from a source or event. This usually takes the form of recording, writing, jotting, paraphrasing, sketching, labelling, outlining, and annotating. Note-taking applications enable users to type, write, and draw on their devices just as they would on paper. Many applications have their own unique features, storage, organization, and sharing capabilities, and it’s up to the user to identify which note-taking application will work best for their needs.
+    
+    A list of note-taking applications that were tested and their features can be found below:
+    
+    Note-Taking Tools at UM
+    
+   
+<h3> How does it work?</h3>    The user will access the applications through the device of their choice, commonly via a laptop or tablet. Some note-taking applications are available cross-platform and on multiple devices while others may be limited to one platform or device. For example, Notability is only available within Apple products whereas OneNote markets their availability across all platforms and devices.
+    
+    Each application contains their own unique set of features which can limit (or exceed) their effectiveness in various scenarios. For example, a STEM student taking notes in class will have distinct needs from an employee who is taking notes during a business meeting. Therefore, the STEM student might want an application that provides rich handwriting tools, equation editors and may invest in a stylus and tablet to take effective notes. The STEM student might also want the application to have dividers and subcategories to organize the classes or projects they work on. The employee on the other hand may want a simpler interface to keep track of minutes using a laptop, and the number of projects will determine the type of categorizing and divisions they might need.
+    
+    <h3></h3>
+    Education – Note-taking is not a new initiative in education. Students are encouraged to take notes on their lectures, class assignments, readings, and other learning activities. Faculty share notes with students to outline and summarize the content in the course. The Office of Disability Services work with faculty, able-bodied, and disabled students to make sure the students have the notes they need to succeed in class. In recent years, students, faculty, and departments around universities are turning over to applications that will allow them to store and share their information more efficiently.</p>
+    </div>
+  )
+}
+
+export default AboutScreen
